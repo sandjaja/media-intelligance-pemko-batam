b1f5b5e8b35fe7621d3eee5a8cb2384022900e93
sandjaja <anrosandjaja@gmail.com>
2026-09-07 16:21:10 +0700

    chore(admin): bust district admin cache

--- admin.js
+++ admin.js
@@ -1,1 +1,1 @@
-(()=>{const s=document.createElement('script');s.src='./admin-core.js?v=20260907-rbac2';document.body.appendChild(s)})();
+(()=>{const s=document.createElement('script');s.src='./admin-core.js?v=20260907-district1';document.body.appendChild(s)})();
