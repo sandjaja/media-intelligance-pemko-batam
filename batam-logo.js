a473e69403c642d4ec2e00aeb595893922c69cb9
sandjaja <anrosandjaja@gmail.com>
2026-09-10 16:09:32 +0700

    fix(branding): use local pure vector crest asset

--- batam-logo.js
+++ batam-logo.js
@@ -1,2 +1,5 @@
-window.BATAM_LOGO_DATA_URI='https://disbudpar.batam.go.id/wp-content/uploads/sites/22/2020/01/pemko-2-1024x724.png';
-window.BATAM_LOGO_URL=window.BATAM_LOGO_DATA_URI;
+(()=>{
+  const official=new URL('./assets/batam-logo-vector.svg',document.baseURI).href;
+  window.BATAM_LOGO_DATA_URI=official;
+  window.BATAM_LOGO_URL=official;
+})();
